c30f5aebbcd3413235c1734d643007c6ce79b89c
DaraMendesBotecchia <101998757+DaraMendesBotecchia@users.noreply.github.com>
2023-04-03 10:12:30 -0300

    Create array.js

---
+++ array.js
@@ -0,0 +1,9 @@
+
+    // Inserindo o array
+    const cores = ['azul', 'vermelho', 'amarelo', 'verde', 'rosa'];
+
+    // Executando o loop for para imprimir cada cor
+    for (let i = 0; i < cores.length; i++) {
+      document.write(`Minha escolha ${i+1} é ${cores[i]} <br>`);
+    }
+
