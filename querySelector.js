4291aabf91e963a07edb8e169232736185d70061
DaraMendesBotecchia <101998757+DaraMendesBotecchia@users.noreply.github.com>
2023-04-24 10:18:40 -0300

    Update querySelector.js

--- querySelector.js
+++ querySelector.js
@@ -1,1 +1,15 @@
+<script defer>
+  const accordionItems = document.querySelectorAll('.accordion-item');
+
+  accordionItems.forEach(item => {
+    const header = item.querySelector('.accordion-header');
+    const content = item.querySelector('.accordion-content');
+
+    header.addEventListener('click', () => {
+      content.classList.toggle('show');
+    });
+  });
+</script>
+
+
 
